5ce8cc3f1d8e9ad32a11804f4cc4da07f43f2435
Udana Wimukthi <74277500+pmuwimukthi@users.noreply.github.com>
2025-10-10 06:31:21 +0530

    Delete app directory

--- app/(tabs)/profile.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function ProfileScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Profile Screen</Text>
-    </View>
-  );
-}
